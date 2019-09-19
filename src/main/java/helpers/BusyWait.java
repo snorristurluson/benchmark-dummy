69f30a14f20527f5fe84b37f0e1d5cb85586599f
Snorri Sturluson <snorri.sturluson@gmail.com>
2019-09-19 20:22:28 +0000

    BusyWait is a little faster

--- src/main/java/helpers/BusyWait.java
+++ src/main/java/helpers/BusyWait.java
@@ -1,7 +1,7 @@
 package helpers;
 
 public class BusyWait implements Benchmark {
-    private static int EXTRAWAIT = 8;
+    private static int EXTRAWAIT = 7;
 
     @Override
     public void run(String param) {
