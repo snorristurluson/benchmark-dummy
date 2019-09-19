f934694b2fbfd282eda5b0ba84f863e8afdcef5d
Snorri Sturluson <snorri.sturluson@gmail.com>
2019-09-19 20:21:25 +0000

    FileMaker creates more files

--- src/main/java/helpers/FileMaker.java
+++ src/main/java/helpers/FileMaker.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 public class FileMaker implements Benchmark {
-    private static int EXTRA = 2;
+    private static int EXTRA = 4;
 
     @Override
     public void run(String param) {
